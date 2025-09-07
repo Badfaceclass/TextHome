e88f908a72fea24a30cb67faa966904b4d193eb2
bueno <justhinvalleclass@gmail.com>
2025-09-07 14:44:27 -0500

    Primera subida1

---
+++ Gitprueba/Gitprueba/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gitprueba
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ingrese su nombre: ");
+            string nombre = Console.ReadLine().ToUpper();
+
+            Console.Write("Ingrese su nombre: ");
+            int edad = Convert.ToInt32(Console.ReadLine());
+        }
+    }
+}
